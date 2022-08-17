RM6785-org/vendor_realme_RM6785

Add libmpbase.so through RM6785-vendor.mk
To avoid conflicts with faceunlock repo
error: vendor/realme/RM6785: MODULE.TARGET.SHARED_LIBRARIES.libmpbase
already defined by external/faceunlock.

RM6785-vendor.mk
@@ -878,7 +878,9 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RM6785/proprietary/vendor/firmware/wifi_19661.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi_19661.cfg \
     vendor/realme/RM6785/proprietary/vendor/firmware/wifi_20682.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi_20682.cfg \
     vendor/realme/RM6785/proprietary/vendor/lib/egl/egl.cfg:$(TARGET_COPY_OUT_VENDOR)/lib/egl/egl.cfg \
-    vendor/realme/RM6785/proprietary/vendor/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+    vendor/realme/RM6785/proprietary/vendor/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
+    vendor/realme/RM6785/proprietary/vendor/lib64/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so \
+    vendor/realme/RM6785/proprietary/vendor/lib/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so 
 
 PRODUCT_PACKAGES += \
     libem_support_jni \
@@ -1468,7 +1470,6 @@ PRODUCT_PACKAGES += \
     libminiui \
     libmmprofile \
     libmnetlink_v104 \
-    libmpbase \
     libmsbc_mtk \
     libmsnr \
     libmtcloader \

xyzedn/proprietary_vendor_realme_RM6785

RM6785/RM6785-vendor.mk
@@ -854,7 +854,9 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RM6785/proprietary/vendor/firmware/tp/20682/LIMIT_NT36672C_NF_TIANMA.img:$(TARGET_COPY_OUT_VENDOR)/firmware/tp/20682/LIMIT_NT36672C_NF_TIANMA.img \
     vendor/realme/RM6785/proprietary/vendor/firmware/wifi_19661.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi_19661.cfg \
     vendor/realme/RM6785/proprietary/vendor/firmware/wifi_20682.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi_20682.cfg \
-    vendor/realme/RM6785/proprietary/vendor/lib/egl/egl.cfg:$(TARGET_COPY_OUT_VENDOR)/lib/egl/egl.cfg
+    vendor/realme/RM6785/proprietary/vendor/lib/egl/egl.cfg:$(TARGET_COPY_OUT_VENDOR)/lib/egl/egl.cfg \
+    vendor/realme/RM6785/proprietary/vendor/lib/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so \
+    vendor/realme/RM6785/proprietary/vendor/lib64/libmpbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmpbase.so
 
 PRODUCT_PACKAGES += \
     libem_support_jni \
@@ -1438,7 +1440,6 @@ PRODUCT_PACKAGES += \
     liblpcnr \
     libmedia_helper-v30 \
     libmfllcore \
-    libmpbase \
     libmsbc_mtk \
     libmsnr \
     libmtcloader \